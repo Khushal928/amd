433545c985b6a64c8784af2bcde3550dcc03f816
Ivin Joel Abraham <ivinjoelabraham@proton.me>
2025-09-30 02:45:48 +0530

    refactor: instrument commands
    Signed-off-by: Ivin Joel Abraham <ivinjoelabraham@proton.me>

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -2,7 +2,7 @@ mod set_log_level;
 
 use crate::commands::set_log_level::set_log_level;
 use serenity::all::RoleId;
-use tracing::trace;
+use tracing::{debug, instrument, trace};
 
 use crate::{
     ids::{FOURTH_YEAR_ROLE_ID, THIRD_YEAR_ROLE_ID},
@@ -22,13 +22,15 @@ async fn is_privileged(ctx: &Context<'_>) -> bool {
 }
 
 #[poise::command(prefix_command)]
+#[instrument(level = "debug", skip(ctx))]
 async fn amdctl(ctx: Context<'_>) -> Result<(), Error> {
-    trace!("Running amdctl command");
     ctx.say("amD is up and running.").await?;
     Ok(())
 }
 
 /// Returns a vector containg [Poise Commands][`poise::Command`]
 pub fn get_commands() -> Vec<poise::Command<Data, Error>> {
-    vec![amdctl(), set_log_level()]
+    let commands = vec![amdctl(), set_log_level()];
+    debug!(commands = ?commands.iter().map(|c| &c.name).collect::<Vec<_>>());
+    commands
 }
